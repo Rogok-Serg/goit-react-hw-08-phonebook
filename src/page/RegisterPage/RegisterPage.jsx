66ed379eed46d83844e15524391d1fe0428cfca7
Rogok-Serg <rogoksv@gmail.com>
2023-08-19 10:33:06 +0300

    Desing Form

--- src/page/RegisterPage/RegisterPage.jsx
+++ src/page/RegisterPage/RegisterPage.jsx
@@ -2,49 +2,105 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { registerUser } from 'redux/authentificated/operations';
 import { selectAuthentificated } from 'redux/authentificated/authSelectors';
+import { Button, Form, Input } from 'antd';
 
 const RegisterPage = () => {
   const authentificated = useSelector(selectAuthentificated);
 
   const dispatch = useDispatch();
 
-  const handleSubmitRegister = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    const name = form.userName.value;
-    const email = form.userEmail.value;
-    const password = form.userPassword.value;
+  // const handleSubmitRegister = e => {
+  //   e.preventDefault();
+  //   const form = e.currentTarget;
+  //   const name = form.userName.value;
+  //   const email = form.userEmail.value;
+  //   const password = form.userPassword.value;
 
-    dispatch(
-      registerUser({
-        name,
-        email,
-        password,
-      })
-    );
-    form.reset();
+  //   dispatch(
+  //     registerUser({
+  //       name,
+  //       email,
+  //       password,
+  //     })
+  //   );
+  //   form.reset();
+  // };
+  const onFinish = values => {
+    dispatch(registerUser(values));
+    console.log('values: ', values);
   };
   if (authentificated) return <Navigate to="/contacts" />;
 
   return (
     <div>
       <h1>Register Your Account</h1>
-      <form onSubmit={handleSubmitRegister}>
-        <p>Name:</p>
-        <label>
-          <input name="userName" type="text" required minLength={5} />
-        </label>
-        <p>Email:</p>
-        <label>
-          <input name="userEmail" type="email" required />
-        </label>
-        <label>
-          <p>Password:</p>
-          <input name="userPassword" type="password" required minLength={7} />
-        </label>
-        <br />
-        <button type="submit">Sing Up</button>
-      </form>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        autoComplete="off"
+        onSubmit={onFinish}
+      >
+        <Form.Item
+          label="Name"
+          name="userName"
+          minLength={5}
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        {/* <input name="userName" type="text" required minLength={5} /> */}
+        <Form.Item
+          label="Email"
+          name="userEmail"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your email!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="userPassword"
+          minLength={7}
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Sing in
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
